4e5dd4ef871d18fa7f6093406c544cce7f219cc5
Hayes Ashby <hayesashby@Hayess-Air.home>
2023-04-09 12:38:35 -0600

    initial overlay of map

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import map from './assets/map.jpeg';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-      <img src={map} />
-      </div>
+    <Card sx={{width: '100vw',
+    height: '100vh',
+    display: 'flex'}}>
+        <CardContent sx={{         width: '100%',
+        height: '100%',       display: 'flex'}}>
+        <div style={{ backgroundImage: `url(${map})`,         backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        display: 'flex',
+        width: '100%',
+        height: '100%' }}>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+      </div>
+        </CardContent>
+      </Card>
   )
 }
 
