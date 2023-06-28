96aef21aae3f54afa2e3d6c59184849008cc17bb
Hayes Ashby <hayes.ashby@usskiandsnowboard.org>
2023-06-28 17:02:29 -0600

    cleanup and starting to mui things

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,11 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Card>
+      <CardHeader title="Title" />
+      <CardContent>Hello All</CardContent>
+    </Card>
+  );
 };
 export default Home;
--- src/pages/Material/data.tsx
+++ src/pages/Material/data.tsx
@@ -1,8 +1,10 @@
+import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+
 const materialData = [
   {
     name: "Southmark",
     HoverInfo: (
-      <div>
+      <Typography>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
         aliquam enim nec est semper, in pellentesque erat ultrices. Pellentesque
         at est sagittis, viverra eros nec, ullamcorper erat. Vestibulum id sem
@@ -11,35 +13,40 @@ const materialData = [
         netus et malesuada fames ac turpis egestas. Nulla urna odio, bibendum in
         finibus eu, dignissim a erat. Mauris lacinia euismod mi, id viverra
         justo hendrerit sit amet. Morbi ut neque massa.
-      </div>
+      </Typography>
     ),
     POIPage: (
       <div>
-        <h1>Lorem Ipsum</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          scelerisque sagittis tortor. Fusce venenatis urna ac ligula posuere,
-          in ultrices arcu efficitur. Duis finibus aliquet felis, in semper eros
-          cursus nec. Sed pretium sapien nec purus interdum, sed malesuada enim
-          molestie. Cras at justo ac diam rhoncus sagittis at vel dui. Praesent
-          semper augue eget quam interdum, id consequat velit varius. Quisque
-          non purus semper, feugiat risus at, pellentesque nunc. Morbi tincidunt
-          ullamcorper sapien, sed eleifend enim.
-        </p>
-        <img
-          // onMouseOver={() => {}}
-          src="/Emerus-Interactive/Lynel.png"
-          width="auto"
-          height="auto"
-        />
-        <p>
-          Proin ac leo ac ligula posuere fermentum. Curabitur vitae sollicitudin
-          tellus, eu eleifend ligula. Mauris fringilla velit eu ligula posuere
-          facilisis. Ut auctor malesuada efficitur. Donec vel sagittis nunc, id
-          luctus nunc. Donec in metus ligula. Pellentesque habitant morbi
-          tristique senectus et netus et malesuada fames ac turpis egestas.
-          Quisque efficitur placerat ipsum vitae auctor.
-        </p>
+        <Card>
+          <CardHeader title="Lorem Ipsum" />
+          <CardContent>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              scelerisque sagittis tortor. Fusce venenatis urna ac ligula
+              posuere, in ultrices arcu efficitur. Duis finibus aliquet felis,
+              in semper eros cursus nec. Sed pretium sapien nec purus interdum,
+              sed malesuada enim molestie. Cras at justo ac diam rhoncus
+              sagittis at vel dui. Praesent semper augue eget quam interdum, id
+              consequat velit varius. Quisque non purus semper, feugiat risus
+              at, pellentesque nunc. Morbi tincidunt ullamcorper sapien, sed
+              eleifend enim.
+            </Typography>
+            <img
+              // onMouseOver={() => {}}
+              src="/Emerus-Interactive/Lynel.png"
+              width="auto"
+              height="auto"
+            />
+            <Typography>
+              Proin ac leo ac ligula posuere fermentum. Curabitur vitae
+              sollicitudin tellus, eu eleifend ligula. Mauris fringilla velit eu
+              ligula posuere facilisis. Ut auctor malesuada efficitur. Donec vel
+              sagittis nunc, id luctus nunc. Donec in metus ligula. Pellentesque
+              habitant morbi tristique senectus et netus et malesuada fames ac
+              turpis egestas. Quisque efficitur placerat ipsum vitae auctor.
+            </Typography>
+          </CardContent>
+        </Card>
       </div>
     ),
     coords: "459,457,7",
@@ -48,7 +55,7 @@ const materialData = [
   {
     name: "Imir_Teras",
     HoverInfo: (
-      <div>
+      <Typography>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
         aliquam enim nec est semper, in pellentesque erat ultrices. Pellentesque
         at est sagittis, viverra eros nec, ullamcorper erat. Vestibulum id sem
@@ -57,12 +64,12 @@ const materialData = [
         netus et malesuada fames ac turpis egestas. Nulla urna odio, bibendum in
         finibus eu, dignissim a erat. Mauris lacinia euismod mi, id viverra
         justo hendrerit sit amet. Morbi ut neque massa.
-      </div>
+      </Typography>
     ),
     POIPage: (
       <div>
-        <h1>Lorem Ipsum</h1>
-        <p>
+        <Typography>Lorem Ipsum</Typography>
+        <Typography>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
           scelerisque sagittis tortor. Fusce venenatis urna ac ligula posuere,
           in ultrices arcu efficitur. Duis finibus aliquet felis, in semper eros
@@ -71,15 +78,15 @@ const materialData = [
           semper augue eget quam interdum, id consequat velit varius. Quisque
           non purus semper, feugiat risus at, pellentesque nunc. Morbi tincidunt
           ullamcorper sapien, sed eleifend enim.
-        </p>
-        <p>
+        </Typography>
+        <Typography>
           Proin ac leo ac ligula posuere fermentum. Curabitur vitae sollicitudin
           tellus, eu eleifend ligula. Mauris fringilla velit eu ligula posuere
           facilisis. Ut auctor malesuada efficitur. Donec vel sagittis nunc, id
           luctus nunc. Donec in metus ligula. Pellentesque habitant morbi
           tristique senectus et netus et malesuada fames ac turpis egestas.
           Quisque efficitur placerat ipsum vitae auctor.
-        </p>
+        </Typography>
       </div>
     ),
     coords: "535,280,7",
