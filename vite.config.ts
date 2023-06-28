668707c206b541c5b36330361d503bd518e31c35
Hayes Ashby <hayes.ashby@usskiandsnowboard.org>
2023-06-28 16:26:39 -0600

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // config options
   plugins: [react({ jsxImportSource: "@emotion/react" })],
+  base: "/Emerus-Interactive/"
 });
