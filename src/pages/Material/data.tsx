2e92d659e8908fa6af9fa568198fe6c7681f344c
Hayes Ashby <hayes.ashby@usskiandsnowboard.org>
2023-06-28 16:56:46 -0600

    lynel and example

--- src/pages/Material/data.tsx
+++ src/pages/Material/data.tsx
@@ -15,42 +15,31 @@ const materialData = [
     ),
     POIPage: (
       <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        aliquam enim nec est semper, in pellentesque erat ultrices. Pellentesque
-        at est sagittis, viverra eros nec, ullamcorper erat. Vestibulum id sem
-        ac nisl scelerisque gravida nec sed orci. Pellentesque congue libero et
-        est auctor varius. Pellentesque habitant morbi tristique senectus et
-        netus et malesuada fames ac turpis egestas. Nulla urna odio, bibendum in
-        finibus eu, dignissim a erat. Mauris lacinia euismod mi, id viverra
-        justo hendrerit sit amet. Morbi ut neque massa. Nullam enim tellus,
-        aliquam at lacus at, consectetur ornare quam. Quisque sed dignissim
-        metus. Duis sed blandit velit. Aliquam nec est molestie, dapibus nisi
-        eget, auctor ligula. Integer ornare aliquet sem eget posuere. Fusce vel
-        sapien blandit, hendrerit augue nec, pellentesque magna. Etiam fringilla
-        scelerisque mollis. Cras tristique imperdiet venenatis. Pellentesque
-        habitant morbi tristique senectus et netus et malesuada fames ac turpis
-        egestas. Aliquam luctus ligula nisl, eget sagittis ipsum porttitor at.
-        Sed volutpat tristique aliquet. Vestibulum semper tristique orci ac
-        pharetra. Nam sit amet tortor dictum, accumsan nulla a, efficitur
-        tortor. Aenean interdum dignissim lacus vitae ullamcorper. Etiam id
-        vulputate est. Nulla facilisi. Proin mattis risus nec dictum iaculis.
-        Suspendisse potenti. Morbi quis bibendum erat, ac rutrum orci. In
-        pellentesque felis vitae urna lacinia lobortis. Fusce sit amet rutrum
-        est. In in fermentum neque, vehicula ultrices turpis. Ut dolor lacus,
-        cursus quis varius quis, bibendum a enim. Sed vel urna diam. Maecenas
-        gravida erat sed risus semper viverra. Sed vel vestibulum ipsum, nec
-        convallis sapien. Suspendisse tempor lectus hendrerit, facilisis ipsum
-        in, blandit nisi. Ut sagittis odio non consequat eleifend. Cras purus
-        mi, blandit ut dolor eget, faucibus feugiat neque. Donec eleifend
-        rhoncus libero sit amet maximus. Sed a convallis tortor, vel suscipit
-        purus. Quisque ac ornare elit. Phasellus dictum nec est ut fringilla.
-        Nunc vel massa at tellus finibus tempus tristique id diam. Fusce purus
-        tortor, cursus nec vehicula in, auctor vitae quam. Integer et euismod
-        ante, eget venenatis turpis. Vivamus suscipit fringilla erat. Cras
-        tincidunt, felis id vulputate feugiat, dolor libero ultricies nisi, ac
-        aliquam orci diam id quam. Praesent nec dui urna. Donec in tortor eu
-        turpis eleifend dignissim ut in quam. Donec a sapien eu enim
-        pellentesque egestas.
+        <h1>Lorem Ipsum</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          scelerisque sagittis tortor. Fusce venenatis urna ac ligula posuere,
+          in ultrices arcu efficitur. Duis finibus aliquet felis, in semper eros
+          cursus nec. Sed pretium sapien nec purus interdum, sed malesuada enim
+          molestie. Cras at justo ac diam rhoncus sagittis at vel dui. Praesent
+          semper augue eget quam interdum, id consequat velit varius. Quisque
+          non purus semper, feugiat risus at, pellentesque nunc. Morbi tincidunt
+          ullamcorper sapien, sed eleifend enim.
+        </p>
+        <img
+          // onMouseOver={() => {}}
+          src="/Emerus-Interactive/Lynel.png"
+          width="auto"
+          height="auto"
+        />
+        <p>
+          Proin ac leo ac ligula posuere fermentum. Curabitur vitae sollicitudin
+          tellus, eu eleifend ligula. Mauris fringilla velit eu ligula posuere
+          facilisis. Ut auctor malesuada efficitur. Donec vel sagittis nunc, id
+          luctus nunc. Donec in metus ligula. Pellentesque habitant morbi
+          tristique senectus et netus et malesuada fames ac turpis egestas.
+          Quisque efficitur placerat ipsum vitae auctor.
+        </p>
       </div>
     ),
     coords: "459,457,7",
@@ -72,42 +61,25 @@ const materialData = [
     ),
     POIPage: (
       <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        aliquam enim nec est semper, in pellentesque erat ultrices. Pellentesque
-        at est sagittis, viverra eros nec, ullamcorper erat. Vestibulum id sem
-        ac nisl scelerisque gravida nec sed orci. Pellentesque congue libero et
-        est auctor varius. Pellentesque habitant morbi tristique senectus et
-        netus et malesuada fames ac turpis egestas. Nulla urna odio, bibendum in
-        finibus eu, dignissim a erat. Mauris lacinia euismod mi, id viverra
-        justo hendrerit sit amet. Morbi ut neque massa. Nullam enim tellus,
-        aliquam at lacus at, consectetur ornare quam. Quisque sed dignissim
-        metus. Duis sed blandit velit. Aliquam nec est molestie, dapibus nisi
-        eget, auctor ligula. Integer ornare aliquet sem eget posuere. Fusce vel
-        sapien blandit, hendrerit augue nec, pellentesque magna. Etiam fringilla
-        scelerisque mollis. Cras tristique imperdiet venenatis. Pellentesque
-        habitant morbi tristique senectus et netus et malesuada fames ac turpis
-        egestas. Aliquam luctus ligula nisl, eget sagittis ipsum porttitor at.
-        Sed volutpat tristique aliquet. Vestibulum semper tristique orci ac
-        pharetra. Nam sit amet tortor dictum, accumsan nulla a, efficitur
-        tortor. Aenean interdum dignissim lacus vitae ullamcorper. Etiam id
-        vulputate est. Nulla facilisi. Proin mattis risus nec dictum iaculis.
-        Suspendisse potenti. Morbi quis bibendum erat, ac rutrum orci. In
-        pellentesque felis vitae urna lacinia lobortis. Fusce sit amet rutrum
-        est. In in fermentum neque, vehicula ultrices turpis. Ut dolor lacus,
-        cursus quis varius quis, bibendum a enim. Sed vel urna diam. Maecenas
-        gravida erat sed risus semper viverra. Sed vel vestibulum ipsum, nec
-        convallis sapien. Suspendisse tempor lectus hendrerit, facilisis ipsum
-        in, blandit nisi. Ut sagittis odio non consequat eleifend. Cras purus
-        mi, blandit ut dolor eget, faucibus feugiat neque. Donec eleifend
-        rhoncus libero sit amet maximus. Sed a convallis tortor, vel suscipit
-        purus. Quisque ac ornare elit. Phasellus dictum nec est ut fringilla.
-        Nunc vel massa at tellus finibus tempus tristique id diam. Fusce purus
-        tortor, cursus nec vehicula in, auctor vitae quam. Integer et euismod
-        ante, eget venenatis turpis. Vivamus suscipit fringilla erat. Cras
-        tincidunt, felis id vulputate feugiat, dolor libero ultricies nisi, ac
-        aliquam orci diam id quam. Praesent nec dui urna. Donec in tortor eu
-        turpis eleifend dignissim ut in quam. Donec a sapien eu enim
-        pellentesque egestas.
+        <h1>Lorem Ipsum</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          scelerisque sagittis tortor. Fusce venenatis urna ac ligula posuere,
+          in ultrices arcu efficitur. Duis finibus aliquet felis, in semper eros
+          cursus nec. Sed pretium sapien nec purus interdum, sed malesuada enim
+          molestie. Cras at justo ac diam rhoncus sagittis at vel dui. Praesent
+          semper augue eget quam interdum, id consequat velit varius. Quisque
+          non purus semper, feugiat risus at, pellentesque nunc. Morbi tincidunt
+          ullamcorper sapien, sed eleifend enim.
+        </p>
+        <p>
+          Proin ac leo ac ligula posuere fermentum. Curabitur vitae sollicitudin
+          tellus, eu eleifend ligula. Mauris fringilla velit eu ligula posuere
+          facilisis. Ut auctor malesuada efficitur. Donec vel sagittis nunc, id
+          luctus nunc. Donec in metus ligula. Pellentesque habitant morbi
+          tristique senectus et netus et malesuada fames ac turpis egestas.
+          Quisque efficitur placerat ipsum vitae auctor.
+        </p>
       </div>
     ),
     coords: "535,280,7",
