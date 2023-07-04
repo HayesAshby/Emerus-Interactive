cffe303adfbb5b9c30e96124294a159f36d05695
Hayes Ashby <hayes.ashby@usskiandsnowboard.org>
2023-07-04 17:49:30 -0600

    readme on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Link, Typography } from "@mui/material";
 
 const Home = () => {
   return (
@@ -9,6 +9,21 @@ const Home = () => {
           <Typography>
             A Place for the world of Emerus to grow and for others to interact
             with
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader title="How to Contribute" />
+        <CardContent>
+          <Typography>
+            <Link href="https://github.com/HayesAshby/Emerus-Interactive">
+              Github
+            </Link>
+            Open a pull request to the project
+          </Typography>
+          <Typography>
+            Modify the appropriate data file you want to contribute to Example:
+            ./src/pages/Material/data.tsx
           </Typography>
         </CardContent>
       </Card>
